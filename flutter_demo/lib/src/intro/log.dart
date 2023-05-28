c456e71645fc87511775e73249e0e03c6809dd8b
Tin <lamdh20.cdsg@saigontech.edu.vn>
2023-05-28 15:24:23 +0700

    one More

--- flutter_demo/lib/src/intro/log.dart
+++ flutter_demo/lib/src/intro/log.dart
@@ -1,17 +1,56 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_demo/src/intro/log3.dart';
+import 'package:flutter_demo/src/pages/home_app.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'log3.dart';
 import 'log1.dart';
 import 'log2.dart';
+
 class IntroPage extends StatefulWidget {
-  const IntroPage({Key? key}):super(key: key);
+  const IntroPage({Key? key}) : super(key: key);
 
   @override
-  State<IntroPage> createState() => _SimpleDialog();
+  State<IntroPage> createState() => _IntroPageState();
 }
 
-class _SimpleDialog extends State<IntroPage> {
+class _IntroPageState extends State<IntroPage> {
+  bool _showIntro = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkIntroStatus();
+  }
+
+  void _checkIntroStatus() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  bool showIntro = prefs.getBool('showIntro') ?? true;
+  setState(() {
+    _showIntro = showIntro;
+  });
+
+  if (!showIntro) {
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(builder: (context) => HomeApp()),
+      (route) => false,
+    );
+  }
+}
+
+  void _setIntroStatus(bool status) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool('showIntro', status);
+    setState(() {
+      _showIntro = status;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (!_showIntro) {
+      return IntroPage1();
+    }
+
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -21,14 +60,14 @@ class _SimpleDialog extends State<IntroPage> {
             width: 200,
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(100),
-                image: const DecorationImage(
+              image: const DecorationImage(
                 image: AssetImage('assets/images/2.png'),
                 fit: BoxFit.cover,
               ),
             ),
           ),
           SizedBox(height: 24),
-         const Text(
+          const Text(
             'Cảm Ơn Bạn Đã Sử Dụng',
             style: TextStyle(
               fontSize: 24,
@@ -44,13 +83,29 @@ class _SimpleDialog extends State<IntroPage> {
             ),
           ),
           SizedBox(height: 24),
-          ElevatedButton(onPressed: (){
-            Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context)=>const IntroPage1()), (route) => false);
-          }, child: const Text('Đọc Hướng Dẫn')),
-            ElevatedButton(onPressed: (){
-            Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context)=>const IntroPage3()), (route) => false);
-          }, child: const Text('Bỏ Qua Và Bắt Đầu Sử Dụng'))
-          
+          ElevatedButton(
+            onPressed: () {
+              _setIntroStatus(false);
+              Navigator.pushAndRemoveUntil(
+                context,
+                MaterialPageRoute(builder: (context) => IntroPage1()),
+                (route) => false,
+              );
+            },
+            child: const Text('Đọc Hướng Dẫn'),
+          ),
+           SizedBox(height: 24),
+          ElevatedButton(
+            onPressed: () {
+              _setIntroStatus(false);
+              Navigator.pushAndRemoveUntil(
+                context,
+                MaterialPageRoute(builder: (context) => HomeApp()),
+                (route) => false,
+              );
+            },
+            child: const Text('Bỏ Qua Và Bắt Đầu Sử Dụng'),
+          )
         ],
       ),
     );
